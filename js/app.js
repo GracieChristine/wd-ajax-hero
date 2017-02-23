2c8485426794182dbcc85872d88a835653f9f391
GracieChristine <graciechristineli@gmail.com>
2017-02-23 08:20:40 -0700

    Upload HTML & JS files

--- js/app.js
+++ js/app.js
@@ -1,25 +1,25 @@
 (function() {
-  'use strict';
+  "use strict";
 
-  const movies = [];
+  let movies = [];
 
-  const renderMovies = function() {
-    $('#listings').empty();
+  let renderMovies = function() {
+    $("#listings").empty();
 
-    for (const movie of movies) {
-      const $col = $('<div>').addClass('col s6');
-      const $card = $('<div>').addClass('card hoverable');
-      const $content = $('<div>').addClass('card-content center');
-      const $title = $('<h6>').addClass('card-title truncate');
+    for (let movie of movies) {
+      let $col = $("<div>").addClass("col s6");
+      let $card = $("<div>").addClass("card hoverable");
+      let $content = $("<div>").addClass("card-content center");
+      let $title = $("<h6>").addClass("card-title truncate");
 
       $title.attr({
-        'data-position': 'top',
-        'data-tooltip': movie.title
+        "data-position": "top",
+        "data-tooltip": movie.title
       });
 
       $title.tooltip({ delay: 50 }).text(movie.title);
 
-      const $poster = $('<img>').addClass('poster');
+      let $poster = $("<img>").addClass("poster");
 
       $poster.attr({
         src: movie.poster,
@@ -29,32 +29,74 @@
       $content.append($title, $poster);
       $card.append($content);
 
-      const $action = $('<div>').addClass('card-action center');
-      const $plot = $('<a>');
+      let $action = $("<div>").addClass("card-action center");
+      let $plot = $("<a>");
 
-      $plot.addClass('waves-effect waves-light btn modal-trigger');
-      $plot.attr('href', `#${movie.id}`);
-      $plot.text('Plot Synopsis');
+      $plot.addClass("waves-effect waves-light btn modal-trigger");
+      $plot.attr("href", `#${movie.id}`);
+      $plot.text("Plot Synopsis");
 
       $action.append($plot);
       $card.append($action);
 
-      const $modal = $('<div>').addClass('modal').attr('id', movie.id);
-      const $modalContent = $('<div>').addClass('modal-content');
-      const $modalHeader = $('<h4>').text(movie.title);
-      const $movieYear = $('<h6>').text(`Released in ${movie.year}`);
-      const $modalText = $('<p>').text(movie.plot);
+      let $modal = $("<div>").addClass("modal").attr("id", movie.id);
+      let $modalContent = $("<div>").addClass("modal-content");
+      let $modalHeader = $("<h4>").text(movie.title);
+      let $movieYear = $("<h6>").text(`Released in ${movie.year}`);
+      let $modalText = $("<p>").text(movie.plot);
 
       $modalContent.append($modalHeader, $movieYear, $modalText);
       $modal.append($modalContent);
 
       $col.append($card, $modal);
 
-      $('#listings').append($col);
+      $("#listings").append($col);
 
-      $('.modal-trigger').leanModal();
+      $(".modal-trigger").leanModal();
     }
   };
 
   // ADD YOUR CODE HERE
+  $("button").click((event) => {
+    event.preventDefault();
+    let searchTerm = $("#search").val();
+
+    $.ajax({
+      method: "GET",
+      url: `http://www.omdbapi.com/?s=${searchTerm}`,
+      dataType: "JSON",
+      success: (data) => {
+        let dataArray = data.Search;
+        let sorted = dataArray.sort((a, b) => {
+          return a.Year - b.Year;
+        });
+
+        for (let i = 0; i < data.Search.length; i++) {
+          let movObj = {
+            id: data.Search[i].imdbID,
+            poster: data.Search[i].Poster,
+            title: data.Search[i].Title,
+            year: data.Search[i].Year
+          };
+          $.ajax({
+            method: "GET",
+            url: `http://www.omdbapi.com/?i=${data.Search[i].imdbID}&plot=short`,
+            dataType: "JSON",
+            success: (data1) => {
+              movObj.plot = data1.Plot;
+              movies.push(movObj);
+            },
+            error: () => {
+              console.log("error");
+            }
+          });
+
+        }
+        renderMovies();
+      },
+      error: () => {
+        console.log("error");
+      }
+    });
+  });
 })();
